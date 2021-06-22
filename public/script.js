4c415643c33d65eeb801bb9bfe390b7842d356f1
Kim Carpico <kcarpico@gmail.com>
2021-06-21 17:51:46 -0700

    working on api integration

--- public/script.js
+++ public/script.js
@@ -1,2 +1,18 @@
-
-
+const options = {
+  // Required: API key
+  key: <WHAT GOES HERE THIS IS THE FRONTEND... NOT API></WHAT>, // REPLACE WITH YOUR KEY !!!
+  // Put additional console output
+  verbose: true,
+  // Optional: Initial state of the map
+  lat: 50.4,
+  lon: 14.3,
+  zoom: 5,
+};
+// Initialize Windy API
+windyInit(options, (windyAPI) => {
+  // windyAPI is ready, and contain 'map', 'store',
+  // 'picker' and other usefull stuff
+  const { map } = windyAPI;
+  // .map is instance of Leaflet map
+  L.popup().setLatLng([50.4, 14.3]).setContent("Hello World").openOn(map);
+});
